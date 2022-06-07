0c9f51823fbda9db949f4f223adf03a83bdbdc5e
ayyldzburak1 <ayyldzburak1@gmail.com>
2022-06-07 21:30:11 +0300

    ödev eklendi

---
+++ kdv-hesaplama/kdv-hesaplama/src/KdvHesaplama.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class KdvHesaplama{
+    public static void main(String[] args) {
+
+
+        double tutar, kdvTutar, kdvOran=0.18, kdvlitutar;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Tutar giriniz :");
+        tutar = inp.nextDouble();
+
+        kdvTutar = tutar * kdvOran;
+        kdvlitutar = tutar + kdvTutar;
+        System.out.println(
+        "KDV'siz tutar : " + tutar+
+        "\nKDV oranı : " + kdvOran+
+        "\nKDV tutarı :" + kdvTutar+
+        "\nKDV'li tutar" + kdvlitutar        );
+
+
+    }
+        }
+                      
